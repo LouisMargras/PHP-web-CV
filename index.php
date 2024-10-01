7b4869f50b584ddff724b42d5d1bdf74265783b4
louis <louis.margras@ynov.com>
2024-10-01 10:13:43 +0200

    index.php/ accueil.hmtl fisrt commit "empty"

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First PHP Page</title>
+</head>
+<body>
+<h1>Welcome to my PHP page!</h1>
+<?php
+echo "<p>Today is " . date("Y-m-d") . "</p>";
+?>
+</body>
+</html>
